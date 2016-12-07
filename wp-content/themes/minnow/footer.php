d029841180b2d1bd0c2835eaff16b243e49745e2
Dream Center <dreamcenter@Marketing-Grant.local>
2016-12-07 15:01:50 -0800

    added all the base files for new wp theme which uses _s and bootstrap

---
+++ wp-content/themes/minnow/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Minnow
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'minnow' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'minnow' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'minnow' ), 'minnow', '<a href="http://underscores.me/" rel="designer">Grant Mats</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
